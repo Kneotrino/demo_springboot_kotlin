d73b1e1f285ba5b0738b7173ff088e7de530512b
Neutrino Kusrorong <kneotrino@yahoo.com>
2019-11-05 17:42:57 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.1.9.RELEASE"
+	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	id("org.asciidoctor.convert") version "1.5.8"
+	kotlin("jvm") version "1.2.71"
+	kotlin("plugin.spring") version "1.2.71"
+	kotlin("plugin.jpa") version "1.2.71"
+}
+
+group = "com.clay4seed"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+val developmentOnly by configurations.creating
+configurations {
+	runtimeClasspath {
+		extendsFrom(developmentOnly)
+	}
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+extra["vaadinVersion"] = "14.0.11"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-integration")
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.vaadin:vaadin-spring-boot-starter")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.springframework.session:spring-session-data-redis")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("mysql:mysql-connector-java")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.springframework.integration:spring-integration-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
+
+tasks.test {
+//	outputs.dir(snippetsDir)
+}
+
+tasks.asciidoctor {
+//	inputs.dir(snippetsDir)
+//	dependsOn(test)
+}
---
+++ src/main/kotlin/com/clay4seed/springboot_kotlin/SpringbootKotlinApplication.kt
@@ -0,0 +1,10 @@
+package com.clay4seed.springboot_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinApplication
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinApplication>(*args)
+}
